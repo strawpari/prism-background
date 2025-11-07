de26e42a44d96fcef6ee23880df820e4a82c45dc
strawpari <parijgabriel@gmail.com>
2025-11-07 15:17:11 -0800

    Initial commit with Prism background

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Prism from './Prism'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Prism />
+  </React.StrictMode>
+)
